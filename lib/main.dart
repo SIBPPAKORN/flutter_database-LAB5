65133f37e6c660e789f9e6c4f3eda454b2f000a5
poy <ghhh7731 @email.com>
2022-09-15 15:28:43 +0700

    p1

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       title: 'sibpakorn_app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
