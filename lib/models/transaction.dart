dbdebeb6362ef30da5f086c3cf811ca5b538accf
poy <ghhh7731 @email.com>
2022-09-16 18:09:24 +0700

    p9

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class transaction {
   String title; //ชื่อรายการ
   double product; //ชื่อสินค้า
-  double price;
+  double price; //ราคาสินค้า
 
   transaction(
       {required this.title, required this.product, required this.price});
