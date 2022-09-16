f28225937ff3bb89edd2985e6ccf22416267b719
poy <ghhh7731 @email.com>
2022-09-16 18:03:24 +0700

    p5

--- lib/providers/transaction_providers.dart
+++ lib/providers/transaction_providers.dart
@@ -1,3 +1,37 @@
-import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
 
-class Transactionprovider with ChangeNotifier {}
+class InformationDB {
+  String dbName;
+
+  InformationDB({required this.dbName});
+
+  Future<Database> openDatabase() async {
+    Directory appDirectory = await getApplicationDocumentsDirectory();
+    String dbLacation = join(appDirectory.path, dbName);
+    print(appDirectory);
+    DatabaseFactory dbFactory = await databaseFactoryIo;
+    Database db = await dbFactory.openDatabase(dbLacation);
+    return db;
+  }
+
+  Future<int> InsertData(Informations statement) async {
+    var db = await openDatabase();
+    var store = intMapStoreFactory.store("expense");
+
+    var keyID = store.add(db, {
+      "title": statement.title,
+      "product": statement.product,
+      "price": statement.price
+    });
+    db.close();
+    return keyID;
+  }
+
+  Future loadAllData() async {
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store("expense");
+    var snapshot = await store.find(db,
+        finder: Finder(sortOrders: [SortOrder(Field.key, false)]));
+    return snapshot;
+  }
+}
