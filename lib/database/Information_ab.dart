249c82212d21997bb78f9700c1fe66bd6e2789d0
poy <ghhh7731 @email.com>
2022-09-16 17:54:52 +0700

    p3

---
+++ lib/database/Information_ab.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class InformationDB {
+  String dbName;
+
+  InformationDB({required this.dbName});
+
+  Future<Database> openDatabase() async {
+    Directory appDirectory = await getApplicationDocumentsDirectory();
+    String dbLacation = join(appDirectory.path, dbName);
+    print(appDirectory);
+    DatabaseFactory dbFactory = await databaseFactoryIo;
+    Database db = await dbFactory.openDatabase(dbLacation);
+    return db;
+  }
+
+  Future<int> InsertData(Informations statement) async {
+    var db = await openDatabase();
+    var store = intMapStoreFactory.store("expense");
+
+    var keyID = store.add(db, {
+      "title": statement.title,
+      "product": statement.product,
+      "price": statement.price
+    });
+    db.close();
+    return keyID;
+  }
+
+  Future loadAllData() async {
+    var db = await this.openDatabase();
+    var store = intMapStoreFactory.store("expense");
+    var snapshot = await store.find(db,
+        finder: Finder(sortOrders: [SortOrder(Field.key, false)]));
+    return snapshot;
+  }
+}
